e2ce8e66bd6fc7f8506ff27a330f0d289e236c8e
savin-dev <niduwarafonseka@gmail.com>
2021-01-20 02:01:44 +0530

    create first component

---
+++ webreact/src/components/login.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Login extends Component{
+    render(){
+        return(
+            <div>
+                <p>welcome to the login page.</p>
+            </div>
+        );
+    }
+}
